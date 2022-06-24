501b9eb73f89e3ea1af64a41405a1bd0ef06031e
Brian Nguyen <briannguyencoco@gmail.com>
2022-06-23 21:11:55 -0500

    removed notValid() and game() functions

--- script.js
+++ script.js
@@ -103,67 +103,7 @@ function round(playerSelection, computerSelection) {
     score++;
 } 
 
-function notValid(userinput) {
-    userinput = userinput.toLowerCase();
-    if(userinput === 'rock' || userinput === 'paper' || userinput === 'scissors') {
-        return false;
-    }
-    else {
-        return true;
-    }
-}
 
-function game() {
-    //var to track the # of wins
-    let wins = 0;
-    //var to hold user input
-    let userinput;
-    //var to hold result of each round
-    let result;
-    //var holds # of losses
-    let loses = 0;
-    //for loop to create a 5 round game
-    for(let i = 0; i < 5; i++) {
-        //gets user input
-        //checks if it is not valid
-        //loops through until given valid input
-        //plays round
-        //if result has the term 'Win', then add 1 to the wins var
-        //if result has the term 'Lose', then add 1 to the loses var
-        userinput = prompt('enter your move: ');
-        while(notValid(userinput)){
-            userinput = prompt('Invalid move! Try again: ');
-        }
-        result = round(userinput, computerPlay());
-        if(result.search('Win') != -1) {
-            wins++;
-        }
-        if(result.search('Lose') != -1) {
-            loses++;
-        }
-        console.log(result);
-        console.log('Win Counter: ' + wins);
-        console.log('');
-
-    }
-       
-
-    //if wins is greater than loses then player wins
-    //if loses greater than wins, player loses
-    //else player and computer tie
-    
-    if(wins > loses) {
-        console.log('Congrats, You Won!');
-    }
-    else if(wins === loses) {
-        console.log('You both tied!');
-    }
-    else {
-        console.log('You lose. Better luck next time :(');
-    }
-    
-   
-}
 
 const div = document.querySelector('.result');
 const div2 = document.querySelector('.score');
